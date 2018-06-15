7ba61349bac70f887da87d14c61a6c404d1c31bd
Antoine SIMÉON <antoine_352hotmail.fr>
2018-06-15 16:31:14 +0200

    Ajout fonction supprimer dans Gestion Réservations

--- UI/ModuleGestionReservations.cs
+++ UI/ModuleGestionReservations.cs
@@ -15,10 +15,10 @@ namespace BoVoyage2.UI
         private static readonly List<InformationAffichage> strategieAffichageEntitesMetier =
             new List<InformationAffichage>
             {
-                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueDossier, "NumeroUniqueDossier", 20),
-                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueClient, "NumeroUniqueClient", 20),
-                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroCarteBancaire, "NumeroUniqueCarteBancaire", 30),
-                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueVoyage, "NumeroUniqueVoyage", 20),
+                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueDossier, "Id Réservation", 20),
+                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueClient, "Id Client", 20),
+                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroCarteBancaire, "Numéro CB", 30),
+                InformationAffichage.Creer<DossiersReservation>(x=>x.NumeroUniqueVoyage, "Id Voyage", 20),
                 InformationAffichage.Creer<DossiersReservation>(x=>x.NombreParticipant, "NombreParticipant", 20),
                 InformationAffichage.Creer<DossiersReservation>(x=>x.PrixTotal, "PrixTotal", 10),
             };
@@ -34,19 +34,19 @@ namespace BoVoyage2.UI
         private void InitialiserMenu()
         {
             this.menu = new Menu("Gestion 1");
-            this.menu.AjouterElement(new ElementMenu("1", "Afficher les reservations")
+            this.menu.AjouterElement(new ElementMenu("1", "Afficher les réservations")
             {
                 FonctionAExecuter = this.AfficherReservations
             });
-            this.menu.AjouterElement(new ElementMenu("2", "Ajouter une reservations")
+            this.menu.AjouterElement(new ElementMenu("2", "Ajouter une réservation")
             {
                 FonctionAExecuter = this.AjouterReservation
             });
-            this.menu.AjouterElement(new ElementMenu("3", "Rechercher une Reservation")
+            this.menu.AjouterElement(new ElementMenu("3", "Rechercher une réservation")
             {
                 FonctionAExecuter = this.RechercherReservation
             });
-            this.menu.AjouterElement(new ElementMenu("4", "Supprimer une reservation")
+            this.menu.AjouterElement(new ElementMenu("4", "Supprimer une réservation")
             {
                 FonctionAExecuter = this.SupprimerReservation
             });
@@ -66,23 +66,23 @@ namespace BoVoyage2.UI
         public void AfficherReservations()
         {
 
-            ConsoleHelper.AfficherEntete("Afficher les reservations");
+            ConsoleHelper.AfficherEntete("Afficher les réservations");
             var liste = Application.GetBaseDonnees().DossiersReservation.ToList();
             ConsoleHelper.AfficherListe(liste, strategieAffichageEntitesMetier);
         }
 
         private void AjouterReservation()
         {
-            ConsoleHelper.AfficherEntete("Ajouter une reservation");
+            ConsoleHelper.AfficherEntete("Ajouter une réservation");
 
             Application.ModuleGestionClients.AfficherClients();
-            var numeroUniqueClient = ConsoleSaisie.SaisirEntierObligatoire("Choisissee le NumeroUniqueClient: ");
+            var numeroUniqueClient = ConsoleSaisie.SaisirEntierObligatoire("Choisissez l'Id du client: ");
             Application.ModuleGestionVoyages.AfficherVoyages();
-            var numeroUniqueVoyage = ConsoleSaisie.SaisirEntierObligatoire(" Choisissez le NumeroUniqueVoyage: ");
+            var numeroUniqueVoyage = ConsoleSaisie.SaisirEntierObligatoire(" Choisissez l'Id du voyage: ");
 
             using (var db = new BaseDonnees())
             {
-                
+
                 var reservation = new DossiersReservation
                 {
                     NumeroCarteBancaire = ConsoleSaisie.SaisirChaineObligatoire("NumeroCarteBancaire : "),
@@ -90,41 +90,39 @@ namespace BoVoyage2.UI
                     PrixTotal = ConsoleSaisie.SaisirEntierObligatoire("PrixTotal: "),
 
                 };
-                var assurance = ConsoleSaisie.SaisirChaineObligatoire("Shouhaitez vous souscrire à l'assurance? 1: Oui 0: Non");
-                if (assurance == "1") 
-                {
-                    reservation.Assurance = true;
-                    Console.WriteLine("Vous avez souscrit à une assurance pour ce voyage");
-                }
-                else
-                {
-                    reservation.Assurance = false;
-                    Console.WriteLine("Vous n'avez pas d'assurance pour ce voyage");
-                }
                 reservation.NumeroUniqueClient = numeroUniqueClient;
                 reservation.NumeroUniqueVoyage = numeroUniqueVoyage;
                 db.DossiersReservation.Add(reservation);
                 db.SaveChanges();
             }
 
-
         }
         private void RechercherReservation()
         {
-            ConsoleHelper.AfficherEntete("Rechercher une reservation");
+            ConsoleHelper.AfficherEntete("Rechercher une réservation");
         }
         private void SupprimerReservation()
         {
-            ConsoleHelper.AfficherEntete("Supprimer une reservation");
+            ConsoleHelper.AfficherEntete("Supprimer une réservation");
+
+            var liste = Application.GetBaseDonnees().DossiersReservation.ToList();
+            ConsoleHelper.AfficherListe(liste, strategieAffichageEntitesMetier);
+
+            using (var db = new BaseDonnees())
+            {
+                var dossierReservation = new DossiersReservation
+                {
+                    NumeroUniqueDossier = ConsoleSaisie.SaisirEntierObligatoire("Entrez l'Id du dossier de réservation à supprimer :")
+                };
+
+                db.DossiersReservation.Attach(dossierReservation);
+                db.DossiersReservation.Remove(dossierReservation);
+                db.SaveChanges();
+            }
         }
 
-        
 
 
 
-
+    }
 }
-}
-    
-
-
